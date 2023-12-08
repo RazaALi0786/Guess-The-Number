d2be28838dd886a5503a7a9956877350be566016
Syed Raza Ali <razaali26296@gmail.com>
2023-12-08 17:31:23 +0530

    Third Commit

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
 "use strict";
-// document.querySelector(".message").textContent = "🎊 Correct Number!";
-// document.querySelector(".number").textContent = 13;
-// document.querySelector(".score").textContent = 10;
-// document.querySelector(".guess").value = 20;
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
@@ -33,4 +29,14 @@ document.querySelector(".check").addEventListener("click", function () {
       document.querySelector(".score").textContent = 0;
     }
   }
+});
+document.querySelector(".again").addEventListener("click", function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector(".message").textContent = "Start guessing...";
+  document.querySelector(".score").textContent = score;
+  document.querySelector(".number").textContent = "?";
+  document.querySelector(".guess").value = "";
+  document.querySelector("body").style.backgroundColor = "#222";
+  document.querySelector(".number").style.width = "15rem";
 });
